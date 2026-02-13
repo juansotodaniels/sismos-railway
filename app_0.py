7d3b55b3ec7d90d0bce35ef7e57a76095ecd0786
juansotodaniels <63661367+juansotodaniels@users.noreply.github.com>
2026-02-13 17:47:38 -0300

    Update app_0.py

--- app_0.py
+++ app_0.py
@@ -722,7 +722,7 @@ def home(n: int = Query(DEFAULT_TABLE_ROWS, ge=1, le=20000)):
           <li><b>Profundidad (km):</b> {evento["Profundidad"]}</li>
           <li><b>Magnitud:</b> {evento["magnitud"]} ({evento.get("mag_type","")})</li>
           <li><b>Referencia:</b> {evento.get("Referencia") or "No disponible"}</li>
-          <li><b>Distancia crítica (dc):</b> {dc_txt}</li>
+          <!-- <li><b>Distancia crítica (dc):</b> {dc_txt}</li> -->
         </ul>
 
         <div style="margin: 10px 0 18px 0;">
